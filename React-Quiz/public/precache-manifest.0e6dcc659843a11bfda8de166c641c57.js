8e01b10d91accde20f90089f10d890fb93381fb1
khatumal321 <khatumal10@gmail.com>
2019-11-06 01:03:35 -0800

    React-Assignment

---
+++ React-Quiz/public/precache-manifest.0e6dcc659843a11bfda8de166c641c57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf4ab70badcd62b53bc6de47a980b2b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dab7eda7fcdb2b85ef0",
+    "url": "/static/css/main.0da98e1d.chunk.css"
+  },
+  {
+    "revision": "db4bcc460fc8941a2eae",
+    "url": "/static/js/2.71384961.chunk.js"
+  },
+  {
+    "revision": "7dab7eda7fcdb2b85ef0",
+    "url": "/static/js/main.2ce06457.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "54bd4fbe1364f85b1b95af1e1b65cf4c",
+    "url": "/static/media/logo.54bd4fbe.png"
+  }
+]);
